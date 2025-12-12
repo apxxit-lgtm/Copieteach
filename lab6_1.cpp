e10f997f236da0b090278f21d7ef6e0a81cdf8a8
Apiwit <Apiwit@gmail.com>
2025-12-12 14:16:54 +0700

    lab6

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,23 @@
 using namespace std;
 
 int main(){
+    int x = 0 , y = 0;
+    int i = 1;
+    int num;
+    while(i<0 || i>0){
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    cin >> num;
+            if(num==0){
+                i = 0;
+            }
+            if(num%2 == 0 && num > 0){
+             x = x+1 ;
+        }
+        if(num%2 != 0){
+            y = y+1;
+        }
+    }
+        cout << "#Even numbers = "<<x<<endl;
+        cout << "#Odd numbers = "<<y;
     return 0;
 }
